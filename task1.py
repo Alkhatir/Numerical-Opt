5426f395d1398d6de850253b11700103a385acf0
Alkhatir <peerhanna501@gmail.com>
2024-05-01 01:06:57 +0200

    Added task1.ipynb and task1.py with 5 problems. Solved the first one using steepst descent.

---
+++ task1.py
@@ -0,0 +1,176 @@
+def function1(x)->float:
+    """
+    x^{3}+3x^{2} +2x+4
+    """
+    return x**3 + 3*x**2 + 2*x + 4
+def function1_derivative(x)->float:
+    """
+    3x^{2}+6x+2
+    """
+    return 3*x**2 + 6*x + 2
+def function1_second_derivative(x)->float:
+    """
+    6x+6
+    """
+    return 6*x + 6
+
+def function2(x) -> float:
+    """
+    x^{3} + 2x^{2} - 5x + 3
+    """
+    return x**3 + 2*x**2 - 5*x + 3
+
+def function2_derivative(x) -> float:
+    """
+    3x^{2} + 4x - 5
+    """
+    return 3*x**2 + 4*x - 5
+
+def function2_second_derivative(x) -> float:
+    """
+    6x + 4
+    """
+    return 6*x + 4
+
+def function3(x) -> float:
+    """
+    4x^{4} - 3x^{3} + x^{2} + 10
+    """
+    return 4*x**4 - 3*x**3 + x**2 + 10
+
+def function3_derivative(x) -> float:
+    """
+    16x^{3} - 9x^{2} + 2x
+    """
+    return 16*x**3 - 9*x**2 + 2*x
+
+def function3_second_derivative(x) -> float:
+    """
+    48x^{2} - 18x + 2
+    """
+    return 48*x**2 - 18*x + 2
+
+def function4(x) -> float:
+    """
+    -x^{5} + 5x^{3} - 3x^{2} + 7
+    """
+    return -x**5 + 5*x**3 - 3*x**2 + 7
+
+def function4_derivative(x) -> float:
+    """
+    -5x^{4} + 15x^{2} - 6x
+    """
+    return -5*x**4 + 15*x**2 - 6*x
+
+def function4_second_derivative(x) -> float:
+    """
+    -20x^{3} + 30x - 6
+    """
+    return -20*x**3 + 30*x - 6
+
+def function5(x) -> float:
+    """
+    2x^{3} - 8x + 4
+    """
+    return 2*x**3 - 8*x + 4
+
+def function5_derivative(x) -> float:
+    """
+    6x^{2} - 8
+    """
+    return 6*x**2 - 8
+
+def function5_second_derivative(x) -> float:
+    """
+    12x
+    """
+    return 12*x
+
+def function6(x) -> float:
+    """
+    x^{4} + 2x^{3} - x + 1
+    """
+    return x**4 + 2*x**3 - x + 1
+
+def function6_derivative(x) -> float:
+    """
+    4x^{3} + 6x^{2} - 1
+    """
+    return 4*x**3 + 6*x**2 - 1
+
+def function6_second_derivative(x) -> float:
+    """
+    12x^{2} + 12x
+    """
+    return 12*x**2 + 12*x
+def function7(x) -> float:
+    """
+    3x^{5} - x^{4} + x^{3} - x^{2} + x - 6
+    """
+    return 3*x**5 - x**4 + x**3 - x**2 + x - 6
+
+def function7_derivative(x) -> float:
+    """
+    15x^{4} - 4x^{3} + 3x^{2} - 2x + 1
+    """
+    return 15*x**4 - 4*x**3 + 3*x**2 - 2*x + 1
+
+def function7_second_derivative(x) -> float:
+    """
+    60x^{3} - 12x^{2} + 6x - 2
+    """
+    return 60*x**3 - 12*x**2 + 6*x - 2
+
+def function8(x) -> float:
+    """
+    -2x^{3} + 3x^{2} + 4x - 7
+    """
+    return -2*x**3 + 3*x**2 + 4*x - 7
+
+def function8_derivative(x) -> float:
+    """
+    -6x^{2} + 6x + 4
+    """
+    return -6*x**2 + 6*x + 4
+
+def function8_second_derivative(x) -> float:
+    """
+    -12x + 6
+    """
+    return -12*x + 6
+
+def function9(x) -> float:
+    """
+    x^{6} + x^{4} - 4x^{2} + 2x - 5
+    """
+    return x**6 + x**4 - 4*x**2 + 2*x - 5
+
+def function9_derivative(x) -> float:
+    """
+    6x^{5} + 4x^{3} - 8x + 2
+    """
+    return 6*x**5 + 4*x**3 - 8*x + 2
+
+def function9_second_derivative(x) -> float:
+    """
+    30x^{4} + 12x^{2} - 8
+    """
+    return 30*x**4 + 12*x**2 - 8
+
+def function10(x) -> float:
+    """
+    5x^{4} - 4x^{3} + 3x^{2} - 2x + 1
+    """
+    return 5*x**4 - 4*x**3 + 3*x**2 - 2*x + 1
+
+def function10_derivative(x) -> float:
+    """
+    20x^{3} - 12x^{2} + 6x - 2
+    """
+    return 20*x**3 - 12*x**2 + 6*x - 2
+
+def function10_second_derivative(x) -> float:
+    """
+    60x^{2} - 24x + 6
+    """
+    return 60*x**2 - 24*x + 6
